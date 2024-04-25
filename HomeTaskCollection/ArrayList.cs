80a06397965b9cf67a401354b8d5e5115d04c6a9
Ali Xudiyev <ali.xudiyev98@gmail.com>
2024-04-25 20:57:13 +0400

    HomeTask Collection Added

---
+++ HomeTaskCollection/ArrayList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTaskCollection
+{
+    internal class ArrayList : IEnumerable
+    {
+        public object[] _arr=new object[0];
+        public int Capacity { get; set; } 
+        public int Count { get; set; }
+        public ArrayList()
+        {
+            Capacity = 4;
+        }
+        public void Add(object value) 
+        {
+            Array.Resize(ref _arr, _arr.Length + 1);
+            _arr[_arr.Length - 1] = value;
+            Count++;
+            if (Capacity < Count) Capacity *= 2;
+        }
+        public void Remove(object value)
+        {
+            int indexRemove=Array.IndexOf(_arr, value);
+            RemoveAt(indexRemove);
+        }
+        public void RemoveAt(int index)
+        {
+            if(index >0 || index <= Count)
+            {
+                for (int i = index; i < _arr.Length - 1; i++)
+                {
+                    _arr[i] = _arr[i + 1];
+                }
+                Array.Resize(ref _arr, _arr.Length - 1);
+               Count--;
+                if(Capacity/2>=Count ) Capacity /= 2;
+            }
+
+        }
+        public void TrimToSize()
+        {
+            if (Capacity > Count)
+                Array.Resize(ref _arr, Capacity = Count);
+        }
+
+        public void AddRange(ICollection collection) 
+        {
+            foreach (var item in collection)
+            {
+                Add(item);
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var item in _arr)
+            {
+                yield return item;
+            }
+        }
+    }
+}
---
+++ HomeTaskCollection/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using HomeTaskCollection;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        ArrayList list = new()
+        {
+            "salam",
+            5,
+            6,
+            "Ali",
+            15,
+            14,
+            8,
+            12,
+            3
+        };
+        Console.WriteLine(list._arr.Length);
+        list.Remove("salam");
+        list.Remove(5);
+
+        //list.TrimToSize();
+
+        //Console.WriteLine(list.Count);
+
+
+        //int[] arr = { 5, 8, 9, 10, 12, 13 };
+        //list.AddRange(arr);
+        //list.Remove(5);
+        //list.Remove(8);
+        //list.Remove(9);
+
+
+        //Console.WriteLine(list.Capacity + " " + list.Count);
+
+        //foreach (var item in list)
+        //{
+        //    Console.WriteLine(item);
+        //}
+    }
+}
